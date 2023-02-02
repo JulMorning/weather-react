f88639292ef34a289d06034e4867d5d2b0249047
Yuliia <106186932+JulMorning@users.noreply.github.com>
2023-02-02 11:54:48 +0200

    Added working Forecast for one day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,38 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  function handleResponse(resonse) {
-    console.log(resonse.data);
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+    console.log(forecast);
   }
-  const apiKey = "16857deadta410be87f0339o286fddad";
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day mb-1">Thu</div>
-
-          <WeatherIcon code="clear-sky-day" size={36} />
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">19˚</span>
-            <span className="WeatherForecast-temp-min">10˚</span>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "16857deadta410be87f0339o286fddad";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
